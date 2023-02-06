972bc320058939c760a2c56d246017a03dce1211
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-02-07 02:11:08 +0300

    deneme

---
+++ src/ifElseBlock.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ifElseBlock {
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+        if (n % 2 == 1) {
+            System.out.println("Weird");
+        }
+        if (n % 2 == 0) {
+            if (n > 1 && n < 6) {
+                System.out.println("Not Weird");
+            }
+            else if (n > 5 && n < 21) {
+                System.out.println("Weird");
+            }
+            else if (n > 20) {
+                System.out.println("Not Weird");
+            }
+        }
+    }
+}
