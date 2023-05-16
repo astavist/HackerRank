f85127e1cfc269bb557f3b1bf7420ac9ad02470a
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-05-16 23:06:20 +0300

    Added stinstout solutions

---
+++ src/St_inSt_out.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class St_inSt_out {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        var a = input.next();
+        System.out.println(a);
+        var b = input.next();
+        System.out.println(b);
+        var c = input.next();
+        System.out.println(c);
+    }
+}
