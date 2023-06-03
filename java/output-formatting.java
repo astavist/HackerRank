7d49735bf503f4cbe80460ce17842b6604cd94e7
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-06-03 23:35:19 +0300

    Added output-formatting

---
+++ java/output-formatting.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+   public static void main(String[] args) {
+                Scanner input=new Scanner(System.in);
+                System.out.println("================================");
+                for(int i=0;i< 3;i++){
+                    String s1=input.next();
+                    int x=input.nextInt();
+                    int a=15-s1.length();
+                    for(int j=0;j< a;j++){
+                        s1=s1+' ';
+                    }String z=Integer.toString(x);
+                    if(z.length()==2)
+                    {
+                        z='0'+z;
+                    }
+                    if(z.length()==1)
+                    {
+                        z="00"+z;
+                    }
+                    System.out.println(s1+z);
+                }
+                System.out.println("================================");
+            }
+}
