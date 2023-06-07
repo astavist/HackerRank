063a45569e3bbd4e77812dd1bbc84457469150f0
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-06-07 16:49:53 +0300

    Added intToString

---
+++ java/intToString.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in .nextInt();
+        in.close();
+
+        String s = Integer.toString(n);
+
+        if (n == Integer.parseInt(s)) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer.");
+        }
+    }
+}
