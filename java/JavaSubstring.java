61e248d3778d811717a43184f0c9b32ac88621a3
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-08-16 19:30:18 +0300

    Added JavaSubstring

---
+++ java/JavaSubstring.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static List<Character> convertStringToCharList(String S) {
+        List<Character> chars = new ArrayList<>();
+        for(char ch : S.toCharArray()) {
+            chars.add(ch);
+        }
+
+        return chars;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+
+        List<Character> chars = convertStringToCharList(S);
+        for(int i = start;i<end;i++){
+            System.out.print(chars.get(i));
+        }
+
+    }
+}
