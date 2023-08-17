964d1d79c59745bf4156671c1c0c0c230b40d4fd
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-08-17 21:40:08 +0300

    Added JavaSubstringComparisons

---
+++ java/JavaSubstingComparisons.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        int k = scanner.nextInt();
+
+        String minSubstring = s.substring(0,k);
+        String maxSubstring = s.substring(0,k);
+
+        for (int i = 1; i < s.length()-k+1; i++) {
+            String sub = s.substring(i,i+k);
+            if (sub.compareTo(minSubstring) < 0) {
+                minSubstring = sub;
+            }
+            if (sub.compareTo(maxSubstring) > 0) {
+                maxSubstring = sub;
+            }
+        }
+        System.out.println(minSubstring+"\n"+maxSubstring);
+    }
+}
