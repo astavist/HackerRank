b61dfec119263885edcb07308e2db6a4d601e991
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-09-01 20:49:52 +0300

    Added JavaBigInteger

---
+++ javaSolutions/src/main/java/JavaBigInteger/JavaBigInteger.java
@@ -0,0 +1,14 @@
+package JavaBigInteger;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class JavaBigInteger {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BigInteger a = new BigInteger(scan.nextLine());
+        BigInteger b = new BigInteger(scan.nextLine());
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
